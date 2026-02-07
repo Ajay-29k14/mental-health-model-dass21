94ebf27719e908cfedd6d4b9b0d9016811f08556
Ajay <ajay.p.14.kumar@gmail.com>
2026-02-07 14:30:39 +0530

    2nd comit

--- src/predict.py
+++ src/predict.py
@@ -2,14 +2,15 @@ import joblib
 import numpy as np
 
 # Load models
-model_dep = joblib.load("../rf_model_depression.pkl")
-model_anx = joblib.load("../rf_model_anxiety.pkl")
-model_str = joblib.load("../rf_model_stress.pkl")
+model_dep = joblib.load("../models/rf_model_depression.pkl")
+model_anx = joblib.load("../models/rf_model_anxiety.pkl")
+model_str = joblib.load("../models/rf_model_stress.pkl")
+
 
 sample_response = [
-    1, 0, 2, 1, 0, 1, 0,
-    2, 1, 0, 1, 0, 1, 1,
-    0, 1, 0, 1, 0, 1, 0
+    1, 0, 0, 0, 0, 1, 0,
+    1, 3, 0, 0, 0, 1, 3,
+    0, 2, 2, 1, 0, 1, 0
 ]
 
 X_new = np.array(sample_response).reshape(1, -1)
